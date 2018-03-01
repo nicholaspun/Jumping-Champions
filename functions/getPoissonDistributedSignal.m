d289174aa0dad2835e0f1596add1f37ea9f4cd32
nicholaspun <nicholaspun99@gmail.com>
2018-02-28 22:07:49 -0800

    move poisson generated signal logic to function

--- functions/getPoissonDistributedSignal.m
+++ functions/getPoissonDistributedSignal.m
@@ -1,7 +1,44 @@
-function sig = getPoissonDistributedSignal(inputArg1,inputArg2)
-%GETPOISSONDISTRIBUTEDSIGNAL Summary of this function goes here
-%   Detailed explanation goes here
+function sig = getPoissonDistributedSignal(seq)
+% getPoissonDistributedSignal: 
+%
+% Inputs:
+%  seq - Array
+%
+% Output:
+%  sig - Array 
+%  
+    seq_without_tails = seq(1:end-500);
+    seq_tails = seq(end-499:end);
     
+    sig_main = genSignal(seq_without_tails);
     
+    sig = [-1 * fliplr(sig_main), sig_main, genSignal(seq_tails)];
+end
+
+function sig = genSignal(seq)
+% genSignal: 
+%
+% Inputs:
+%  seq - Array
+%
+% Output:
+%  sig - Array 
+% 
+    sig_len = 0;
+    sig = [];
+
+    seq_min = min(seq);
+    seq_max = max(seq);
+    seq_len = length(seq);
+    
+    while (sig_len ~= seq_len)
+        try_new_number = seq_min + (seq_max - seq_min) * rand;
+        nums_in_interval = length(seq((seq >= try_new_number - 100) & (seq <= try_new_number + 100)));
+        if (rand > (nums_in_interval / 200)) 
+            continue;
+        end
+        sig = unique([sig try_new_number]);
+        sig_len = length(sig);
+    end
 end
 
--- scripts/poisson_signal_generation.m
+++ scripts/poisson_signal_generation.m
@@ -12,41 +12,10 @@ lp_length = num_primes + tails;
 % DO NOT CHANGE! 
 
 %% Implementation:
-primes = nthprime(1:dom_primes); 
-sig_len = 0;
-seq_min = min(primes);
-seq_max = max(primes);
-poisson_seq = [];
-while (sig_len ~= dom_primes)
-    try_new_number = seq_min + (seq_max - seq_min) * rand;
-    nums_in_interval = length(primes((primes >= try_new_number - 100) & (primes <= try_new_number + 100)));
-    if (rand > (nums_in_interval / 200)) 
-        continue;
-    end
-    poisson_seq = unique([poisson_seq try_new_number]);
-    sig_len = length(poisson_seq);
-end
+extended_domain = getPoissonDistributedSignal(nthprime(1:num_primes));
+disp(length(extended_domain));
+sample_domain = extended_domain(501:end-500);
 
-prime_tails = nthprime(dom_primes + 1:num_primes);
-sig_len = 0;
-seq_min = min(prime_tails);
-seq_max = max(prime_tails);
-poisson_seq_tails = []; 
-while (sig_len ~= 500)
-    try_new_number = seq_min + (seq_max - seq_min) * rand;
-    nums_in_interval = length(prime_tails((prime_tails >= try_new_number - 100) & (prime_tails <= try_new_number + 100)));
-    if (rand > (nums_in_interval / 200)) 
-        continue;
-    end
-    poisson_seq_tails = unique([poisson_seq_tails try_new_number]);
-    sig_len = length(poisson_seq_tails);
-end
-
-extended_domain = [-1 * fliplr(poisson_seq(1:500)), poisson_seq, poisson_seq_tails];
-
-% Construction of prime lattices and other arrays:
-
-sample_domain = poisson_seq; % Define the actual domain that the primes run through (without the tails)
 samps = ones(1, dom_primes); % set the amplitudes 
 new_domain = 1:max(sample_domain); % Define the new domain (Every integer value from 1 to the highest prime)
 d_extended_domain = calc_t_prime(extended_domain); % Derivative Lattice 
