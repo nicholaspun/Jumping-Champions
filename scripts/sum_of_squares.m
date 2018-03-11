f7621662627f1644c3c5730e12b31ccee723c22d
nicholaspun <nicholaspun99@gmail.com>
2018-03-11 00:47:51 -0800

    create sums of squares sequence + figure

--- scripts/sum_of_squares.m
+++ scripts/sum_of_squares.m
@@ -1,7 +1,7 @@
 %% Description: 
 
 %% Useful Constants: 
-maxSquare = 100; 
+maxSquare = 150; 
 
 %% Implementation:
 seq = getSumsOfSquaresSequence(maxSquare);
@@ -22,7 +22,22 @@ poisson_sig = getReconstructedSignalFromSequence(poisson_seq);
 fft_poisson_arr = mfft(poisson_sig);
 
 %% Figures:
-figure(1); plot(sig);
-figure(2); plot(fft_arr); xlim([0 length(fft_arr)]);
-figure(3); plot(poisson_sig);
-figure(4); plot(fft_poisson_arr); xlim([0 length(fft_poisson_arr)]);
+figure(1); 
+subplot(2,1,1);
+plot(sig);
+set(gca,'FontSize',24);
+subplot(2,1,2);
+plot(poisson_sig);
+set(gca,'FontSize',24);
+
+figure(2); 
+subplot(2,1,1);
+plot(fft_arr);
+set(gca,'FontSize',24);
+xlim([length(fft_arr)/2 length(fft_arr)]);
+subplot(2,1,2);
+plot(fft_poisson_arr);
+set(gca,'FontSize',24);
+xlabel("Frequency");
+ylabel("Amplitude");
+xlim([length(fft_poisson_arr)/2 length(fft_poisson_arr)]);
